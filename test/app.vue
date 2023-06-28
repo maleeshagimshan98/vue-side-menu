89f4e9d306165a3e3d9dba7414b92bbb5922771c
Maleesha Gimshan <maleeshagimshan74@gmail.com>
2023-06-28 23:09:23 +0530

    changed the testing style to be inline with other UI Components

---
+++ test/app.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="app">
+        <app :items="items"></app>
+    </div>
+</template>
+
+<script>
+import app from "../src/side_menu.vue"
+
+export default {
+    data() {
+        let items = {
+            home : {
+                routeName : "home"
+            },
+            profile : {
+                title : 'User Profile',
+                path : "/profile",
+                children : {
+                    edit : {
+                        title : "Edit profile",
+                        path : "/profile/edit",
+                        params : {
+                            userId : 1100,
+                            name : "test"
+                        }
+                    }
+                }
+            }
+        }
+        return {
+            items : items
+        }
+    },
+    methods: {},
+    components: {
+        app
+    }
+}
+</script>
+>
+
+<style>
+.app {
+    margin: 10%;
+    padding: 10%
+}
+</style>
